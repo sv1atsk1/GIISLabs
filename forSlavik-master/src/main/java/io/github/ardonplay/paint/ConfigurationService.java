d72a61559101cbd80676caf655e75ff7e803b9a8
Viachaslau <127544303+sv1atsk1@users.noreply.github.com>
2024-03-04 20:29:18 +0300

    Add files via upload

---
+++ forSlavik-master/src/main/java/io/github/ardonplay/paint/ConfigurationService.java
@@ -0,0 +1,10 @@
+package io.github.ardonplay.paint;
+
+import javafx.scene.image.PixelWriter;
+
+public class ConfigurationService {
+    public static PixelWriter pixelWriter;
+    public ConfigurationService(){
+
+    }
+}
